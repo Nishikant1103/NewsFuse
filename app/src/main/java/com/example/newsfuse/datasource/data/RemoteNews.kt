7798b8d065c472a97316b0d903a5284b4b5eb271
Nishikant Choudhary <nishikant.choudhary@virtual-solution.com>
2026-02-21 23:06:12 +0100

    Remove usage of UUID
    Use automated generated id for News data instead of UUId as its a cleaner approach.
    
    bugfix: nfuse_239665

---
+++ app/src/main/java/com/example/newsfuse/datasource/data/RemoteNews.kt
@@ -0,0 +1,10 @@
+package com.example.newsfuse.datasource.data
+
+data class RemoteNews(
+    val datePosted: String?,
+    val newsTitle: String,
+    val newsDescription: String,
+    val newsLink: String,
+    val newsImageLink: String,
+)
+
